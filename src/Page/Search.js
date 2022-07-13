a94741c9a5e30ea31a135f42bc589b7a23157ef2
Phuong0210 <phuong.ho021021@gmail.com>
2022-07-13 10:54:03 +0700

    new chănge

--- src/Page/Search.js
+++ src/Page/Search.js
@@ -11,7 +11,8 @@ const List=()=>{
       image:""
   }]);  //cars là 1 mảng các đối tượng
   const [noDataFound,setNoDataFound]=useState("");
-  //const [search, setSearch] = useState();
+  const [search, setSearch] = useState();
+ 
   //gọi hàm này khi CarList được render lần đầu (chú ý tham số thứ 2 là mảng rỗng). Hàm này như hàm lifecycle componentDidMount()
   useEffect(() => {
     getProducts();
@@ -25,7 +26,7 @@ const List=()=>{
           if(res.status===200){
               setProducts(res.data.data?res.data.data:[]); //c là biến tự đặt tên gì cũng đc, ko có nó thì in ra cars=[]
               console.log(res.data.data?res.data.data:[]);
-              console.log(cars);//vì cars lúc này chỉ render 
+              console.log(t_restaurants);//vì cars lúc này chỉ render 
           };
           if(res.data.status==="failed" && res.data.success===false){
               setNoDataFound(res.data.message);
@@ -38,22 +39,25 @@ const List=()=>{
 
    
    
-//    const handleSearch = async (e,search) => {
-//     const res = await axios.get(`http://127.0.0.1:8000/api/show?search=${search}`);
-//     // const carList = await res.data;
+   const handleSearch = async (e,search) => {
+    const res = await axios.get(`http://127.0.0.1:8000/api/show?search=${search}`);
+    // const carList = await res.data;
  
     setProducts(res.data.data);
 
-
+   }
+   
 
         return (
             <div className="container h-100">
         <div className="d-flex justify-content-center h-100">
           <div className="searchbar">
-            <input className="search_input" type="text" name placeholder="Search..." />
-            <a href="#" className="search_icon"><i className="fas fa-search" /></a>
+            <input className="search_input" type="text" name="search" placeholder="Search by name..." onChange={(e) => setSearch(e.target.value)} />
+            <input className="search_input" type="text" name="search" placeholder="Search by price..." onChange={(e) => setSearch(e.target.value)} />
+            <button class="" type='' onClick={(e) => handleSearch(e,search)}><i className="fas fa-search" /></button>
           </div>
         </div>
+        <div className='show'>
         {!!t_restaurants
                   ?
                   t_restaurants.map((rest,index)=>
@@ -61,9 +65,9 @@ const List=()=>{
         <div className="card" style={{width: '18rem'}}>
         <img className="card-img-top" src={`http://127.0.0.1:8000/image/${rest.image}`} alt="Card image cap" />
         <div className="card-body">
-          <h5 className="card-title">{rest.pro_name}</h5>
-          <h5 className="card-title">{rest.price}</h5>
-          <p className="card-text">{rest.description}</p>
+          <h5 className="card-title">Tên: {rest.pro_name}</h5>
+          <h5 className="card-title">Giá: {rest.price}</h5>
+          <p className="card-text">Mô tả: {rest.description}</p>
           <a href="#" className="btn btn-primary">Mua ngay</a>
         </div>
       </div>
@@ -72,6 +76,7 @@ const List=()=>{
       :
        
        <div>No data</div> }
+       </div>
       </div>
         );
     
